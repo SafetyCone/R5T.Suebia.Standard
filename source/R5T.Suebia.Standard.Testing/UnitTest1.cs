ee536426b20d01efcd54dcb22c463075acce4d41
MinexAutomation <minex.automation@gmail.com>
2020-02-20 01:00:21 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Suebia.Standard.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Suebia.Standard.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
