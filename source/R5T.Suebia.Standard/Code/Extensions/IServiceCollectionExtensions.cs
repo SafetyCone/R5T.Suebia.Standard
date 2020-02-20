668686a3ea96fe732673be6d17954153c1cd869e
MinexAutomation <minex.automation@gmail.com>
2020-02-20 01:10:49 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Suebia.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Alamania.Standard;
+using R5T.Dacia;
+using R5T.Lombardy.Standard;
+
+using R5T.Suebia.Alamania;
+
+
+namespace R5T.Suebia.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="ISecretsDirectoryPathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddSecretsDirectoryPathProvider(this IServiceCollection services)
+        {
+            services.AddAlamaniaSecretsDirectoryPathProvider(
+                services.AddRivetOrganizationDirectoryPathProviderAction(),
+                services.AddStringlyTypedPathOperatorAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ISecretsDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ISecretsDirectoryPathProvider> AddSecretsDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ISecretsDirectoryPathProvider>(() => services.AddSecretsDirectoryPathProvider());
+            return serviceAction;
+        }
+    }
+}
